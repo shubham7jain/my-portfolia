ebbbe0c964db74f8302e102f2307ea09120bc92d
shubham7jain <shubham7jain@gmail.com>
2019-11-17 01:49:22 -0800

    Add lambda function to extract and upload to s3 bucket

---
+++ upload-portfolio-lambda.py
@@ -0,0 +1,22 @@
+import boto3
+import StringIO
+import zipfile
+import mimetypes
+
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
+
+    portfolio_bucket = s3.Bucket('portfolio.shubham7jain.com')
+    build_bucket = s3.Bucket('portfoliobuild.shubham7jain.com')
+    
+    portfolio_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+    	for nm in myzip.namelist():
+    		obj = myzip.open(nm)
+    		portfolio_bucket.upload_fileobj(obj, nm,
+    		    ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+    		portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    		
+    return 'Hello from lambda'
